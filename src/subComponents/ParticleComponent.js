7e64eaa70892e4a250686c04aea6583e06d43fd6
Alexander Rebai <alexander.rebai@student.hogent.be>
2023-02-14 11:36:39 +0100

    changed react-particles-js dependency to react-particles + tsparticles

--- src/subComponents/ParticleComponent.js
+++ src/subComponents/ParticleComponent.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import styled from 'styled-components';
-import {Particles} from 'react-particles-js';
+
+import Particles from 'react-particles';
+import {loadFull} from 'tsparticles';
+
 //particle configs
 import configDark from '../config/particlesjs-config.json';
 import configLight from '../config/particlesjs-config-light.json';
@@ -15,11 +18,26 @@ const Box = styled.div`
 `;
 
 const ParticleComponent = props => {
+  const particlesInit = useCallback (async engine => {
+    console.log (engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull (engine);
+  }, []);
+
+  const particlesLoaded = useCallback (async container => {
+    await console.log (container);
+  }, []);
+
   return (
     <Box>
       <Particles
+        id="tsparticles"
         style={{position: 'absolute', top: 0}}
         params={props.theme === 'light' ? configLight : configDark}
+        init={particlesInit}
+        loaded={particlesLoaded}
       />
     </Box>
   );
